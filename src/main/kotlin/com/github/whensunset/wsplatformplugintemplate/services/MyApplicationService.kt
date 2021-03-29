ca2150d09c9ba5565686ae36e158d2d51b6b7057
GitHub Action <action@github.com>
2021-03-29 09:08:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/whensunset/wsplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.whensunset.wsplatformplugintemplate.services
+
+import com.github.whensunset.wsplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/whensunset/wsplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/whensunset/wsplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.whensunset.wsplatformplugintemplate.services
 
+import com.github.whensunset.wsplatformplugintemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
